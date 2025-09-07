cbfbcc88b8f62b4334d1a883a51ba998e608ab4e
Hyun <itsmine096@gmail.com>
2025-09-08 02:08:47 +0900

    Boj 8958 다시풀기

--- Boj8958/Boj8958.py
+++ Boj8958/Boj8958.py
@@ -1,13 +1,16 @@
+import sys
+
+
+input = lambda: sys.stdin.readline().rstrip()
+
 n = int(input())
 
 for i in range(n):
-    str = input()
-    str_arr = list(str)
-
-    continuity = 1  # 연속으로 맞춘 횟스
+    l = input()
+    continuity = 1  # 연속으로 맞춘 횟수
     total_score = 0  # 총 점수
-    for isAnswer in str_arr:
-        if isAnswer == 'O':
+    for isAnswer in l:
+        if isAnswer == "O":
             total_score += continuity
             continuity += 1
         else:
